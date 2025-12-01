a91c5ff6f69e5cd56d63ffab84b822ff0770cd53
eshaanchatrath121 <eshaanchatrath121@gmail.com>
2025-12-01 12:23:56 -0500

    Initial Commit

---
+++ EaglePark/EagleParkApp.swift
@@ -0,0 +1,17 @@
+//
+//  EagleParkApp.swift
+//  EaglePark
+//
+//  Created by Eshaan Chatrath on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct EagleParkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
